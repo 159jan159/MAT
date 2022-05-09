c47eb51693aa2584a4f551cad0504377f32c9639
159jan159 <159honza159@gmail.com>
2022-05-09 22:12:23 +0200

    2048

---
+++ MAT_P_2048/src/pkg2048/Main.java
@@ -0,0 +1,10 @@
+package pkg2048;
+
+public class Main {
+
+    public static void main(String[] args) {
+        View v = new View();
+        v.setVisible(true);
+    }
+    
+}
---
+++ MAT_P_2048/src/pkg2048/View.java
@@ -0,0 +1,47 @@
+package pkg2048;
+
+public class View extends javax.swing.JFrame {
+
+    public View() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        platno1 = new pkg2048.Platno();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout platno1Layout = new javax.swing.GroupLayout(platno1);
+        platno1.setLayout(platno1Layout);
+        platno1Layout.setHorizontalGroup(
+            platno1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 520, Short.MAX_VALUE)
+        );
+        platno1Layout.setVerticalGroup(
+            platno1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 564, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(platno1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(platno1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private pkg2048.Platno platno1;
+    // End of variables declaration//GEN-END:variables
+}
